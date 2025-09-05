ad8e4f78c654ebc942c7a69269e7d17e2268962a
Michael Winsor <mgwinsor@gmail.com>
2025-09-05 20:26:07 +0800

    refactor: separate api server file

--- internal/primary/api/user_handler.go
+++ internal/primary/api/user_handler.go
@@ -5,32 +5,8 @@ import (
 	"errors"
 	"net/http"
 
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
 	"github.com/mgwinsor/weekbyweek/internal/app/user"
 )
-
-type Server struct {
-	userService user.Service
-}
-
-func NewServer(service user.Service) *Server {
-	return &Server{
-		userService: service,
-	}
-}
-
-func (s *Server) RegisterRoutes() http.Handler {
-	r := chi.NewRouter()
-	r.Use(middleware.Logger)
-	r.Use(middleware.Recoverer)
-
-	r.Route("/users", func(r chi.Router) {
-		r.Post("/", s.handleCreateUser)
-	})
-
-	return r
-}
 
 func (s *Server) handleCreateUser(w http.ResponseWriter, r *http.Request) {
 	var req user.CreateUserRequest
